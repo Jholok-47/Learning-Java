ab442526ce7d8cb561d82ff697ed1735570a540c
Jholok <zhalak47@gmail.com>
2026-01-21 17:50:03 +0600

    Add string_operations and basics.

---
+++ src/Basics.java
@@ -0,0 +1,21 @@
+public class Basics {
+    public static void main(String[] args) {
+        // Print "Hello, World!" to the console
+        System.out.println("Hello, World!");
+
+        // Declare and initialize variables
+        int number = 10;
+        double decimalNumber = 5.5;
+        boolean isJavaFun = true;
+        String text = "Java Basics";
+
+        // Perform basic arithmetic operations
+        int sum = number + 5;
+        double product = decimalNumber * 2;
+
+        // Print results to the console
+        System.out.println("Sum: " + sum);
+        System.out.println("Product: " + product);
+        System.out.println("Text: " + text);
+    }
+}
--- src/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("This is a test class.");
-        for (int j = 1; j <= 3; j++) {
-            System.out.println("j = " + j);
-        }
-    }
-}
