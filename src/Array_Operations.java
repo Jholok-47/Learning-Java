e5f1a470bb8fc0d0eacf79633592cb049bbd4ac0
Jholok <zhalak47@gmail.com>
2026-01-21 20:14:37 +0600

    Further updated Array_Operations

--- src/Array_Operations.java
+++ src/Array_Operations.java
@@ -2,7 +2,22 @@ public class Array_Operations {
     public static void main(String[] args) {
         int [] numbers = {5, 3, 8, 1, 2};
         int [] arr = new int[5];
+        int [][] arr2 = new int[3][4];
 
+        System.out.println(arr[2] + "\n"); // Accessing the third element
 
+        for (int i = 0; i < 5; i++) {
+            System.out.println(numbers[i]);
+        }
+
+        System.out.println("");
+
+        for(int i=0; i < arr2.length; i++) {
+            for(int j=0; j < arr2[i].length; j++) {
+                arr2[i][j] = i + j;
+                System.out.print(arr2[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
