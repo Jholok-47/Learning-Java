1857fe381f5bf06ffb68a8f451bff66a87ff0f12
Jholok <zhalak47@gmail.com>
2026-01-22 14:55:19 +0600

    Print some triangles

--- src/Patterns.java
+++ src/Patterns.java
@@ -1,19 +1,41 @@
 public class Patterns {
-    public static void main(String[] args) {
-        for(int i = 9; i > 0; i--){
-            for(int j = i; j > 0; j--){
+    // Method for printing a numerical void triangle
+    public static void nvoid(int a) {
+        for (int i = a; i > 0; i--) {
+            for (int j = i; j > 0; j--) {
                 System.out.print(i);
             }
-            for(int k = 0; k < 9 - i; k++){
+            for (int k = 0; k < a - i; k++) {
                 System.out.print(" ");
             }
-            for(int k = 0; k < 9 - i; k++){
+            for (int k = 0; k < a - i; k++) {
                 System.out.print(" ");
             }
-            for(int j = i; j > 0; j--){
+            for (int j = i; j > 0; j--) {
                 System.out.print(i);
             }
-            System.out.println("\n");
+            System.out.println("");
         }
+    }
+
+    // Method to print a lower left numerical triangle
+    public static void ll_triangle(int a){
+        for(int i = 0; i < a; i++){
+            for(int j = 0; j <= i; j++){
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        int x = 9;
+        nvoid(x);
+        System.out.println("");
+
+        ll_triangle(x);
+        System.out.println("");
     }
 }
