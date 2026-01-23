c77db785ea254c30d2b116dee98dd10a3b7d1eeb
Jholok <zhalak47@gmail.com>
2026-01-23 23:22:41 +0600

    Add User_Input file

--- src/Test.java
+++ src/Test.java
@@ -1,27 +1,15 @@
+import java.util.Scanner;
+
 public class Test {
-    public static void armstrong(int n) {
-        int x = n;
-        int digit;
-        int arm = 0;
-        int count = 0;
-        while(x / 10 != 0){
-            x = x / 10;
-            count++;
+    // Method to print a 1 dimensional array of integers
+    public static void print_array(int[] arr) {
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
         }
-        x = n;
-        for(int i = 0; i <= count; i++){
-            digit = x % 10;
-            x /= 10;
-            arm += Math.pow(digit, count+1);
-        }
-        if(arm == n)
-            System.out.println("True");
-        else System.out.println("False");
+        System.out.println("");
     }
 
-
     public static void main(String[] args) {
-       int x = 371;
-        armstrong(x);
+        System.out.println("Hello world");
     }
 }
---
+++ src/User_Input.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class User_Input {
+    // Method to print a 1 dimensional array of integers
+    public static void print_array(int[] arr) {
+        for(int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("");
+    }
+
+    // Method to take a 1 dimensional array as user input
+    public static int[] input_1D_array(Scanner sc) {
+        int arr_size;
+        System.out.print("Enter size of an array: " );
+        arr_size = sc.nextInt();
+        int[] arr = new int[arr_size];
+
+        System.out.print("Enter " + arr_size + " integers: ");
+        for(int i = 0; i < arr_size; i++){
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        print_array(input_1D_array(sc));
+    }
+}
