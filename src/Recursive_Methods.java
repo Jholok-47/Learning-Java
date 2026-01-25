ce3aeaba4af5d76911cd3a748131d9f33a17e2f8
Jholok <zhalak47@gmail.com>
2026-01-25 22:26:21 +0600

    Add Recursive_Methods

---
+++ src/Recursive_Methods.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Recursive_Methods {
+    public static int sum(int s, int i, int n) {
+            if(i > n) {
+                return s;
+            }
+            s += i;
+            return sum(s, i + 1, n);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a positive integer: ");
+        int n = sc.nextInt();
+        System.out.println("Enter a number greater than the first number: ");
+        int m = sc.nextInt();
+        int result = sum(0, n, m);
+        System.out.println("The sum from " + n + " to " + m + " is: " + result);
+    }
+}
--- src/Test.java
+++ src/Test.java
@@ -3,19 +3,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Test {
-    public static int countResponseTimeRegressions(List<Integer> responseTimes) {
-        int avg;
-        int count = 0;
-        for(int i = 1; i < responseTimes.size(); i++){
-            int total = 0;
-            for(int j = i - 1; j >= 0; j--){
-                total += responseTimes.get(j);
-            }
-            avg = total/i;
-            if(responseTimes.get(i) > avg)
-                count++;
+    public static void print_array(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
-        return count;
+        System.out.println("");
     }
 
     public static void main(String[] args) {
@@ -24,11 +16,14 @@ public class Test {
         System.out.println("How many response times will you enter?");
         int n = sc.nextInt();
         System.out.println("Enter the response times: ");
-        for(int i = 0; i < n; i++){
+
+        for (int i = 0; i < n; i++) {
             responseTimes.add(sc.nextInt());
         }
 
-        int regressions = countResponseTimeRegressions(responseTimes);
-        System.out.println("Number of response time regressions: " + regressions);
+        System.out.print("The response times you entered are: ");
+        for (int time : responseTimes) {
+            System.out.print(time + " ");
+        }
     }
 }
